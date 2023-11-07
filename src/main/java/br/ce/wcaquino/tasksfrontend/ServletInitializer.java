5d6d2339b8e5eedc3c30478972e2ddf4466f8332
clodoaldo.zeferini.enterprise <clodoaldo.zeferini.enterprise@outlook.com>
2023-11-07 16:25:34 -0300

    Atualizando Maven

--- src/main/java/br/ce/wcaquino/tasksfrontend/ServletInitializer.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/ServletInitializer.java
@@ -3,9 +3,6 @@ package br.ce.wcaquino.tasksfrontend;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
